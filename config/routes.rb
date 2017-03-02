c15af5bf502d0e0abf518ade6e430172cf581bf7
= <=>
2017-03-01 21:56:09 -0300

    subiendo a Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'paginas/home'
+  get  'paginas/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'paginas#home'
-
+  get 'about' => 'paginas#about'
+  get 'yo' => 'paginas#yo'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
